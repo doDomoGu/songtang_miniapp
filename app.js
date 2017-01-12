e7ccb0ba6729e3b477a22a8378a48bd468ecb043
doDomoGu <kaka_71936410@qq.com>
2017-01-13 04:29:25 +0800

    获取 openid

--- app.js
+++ app.js
@@ -2,29 +2,64 @@
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
+    /*var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    wx.setStorageSync('logs', logs)*/
   },
   getUserInfo:function(cb){
     var that = this
     if(this.globalData.userInfo){
       typeof cb == "function" && cb(this.globalData.userInfo)
     }else{
+      var code;
+      var encryptedData;
+      var iv;
       //调用登录接口
       wx.login({
-        success: function () {
+        success: function (res) {
+          console.log(res);
+          code = res.code;
+          
+
+
+
           wx.getUserInfo({
             success: function (res) {
               that.globalData.userInfo = res.userInfo
+              console.log(res);
+              encryptedData = res.encryptedData;
+              iv = res.iv;
               typeof cb == "function" && cb(that.globalData.userInfo)
+
+              wx.request({
+            url: that.globalData.requestHost+"/ucenter/login/index", //仅为示例，并非真实的接口地址
+            method: "POST",
+            data: {
+              code : code,
+              encryptedData :encryptedData,
+              iv: iv
+            },
+            header: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            success: function(res) {
+              
+              console.log(res.data)
             }
           })
+            }
+          })
+
+          
         }
       })
     }
   },
   globalData:{
-    userInfo:null
+    userInfo:null,
+    requestHost:"http://api.localsongtang.net",
+    //requestHost:"https://api.songtang.net",
+    appid : "wxfeb4bdcd2e97f17b",
+    appsecret : "558f4d98ab4a03e9ccb5e20270806436",
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -21,6 +21,19 @@ Page({
       that.setData({
         userInfo:userInfo
       })
+      //console.log(userInfo.encryptedData);
     })
+    
+    wx.request({
+      url: getApp().globalData.requestHost+'/ucenter/users', //仅为示例，并非真实的接口地址
+      data:{},
+      header: {
+          'content-type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res.data)
+      }
+    })
+
   }
 })
