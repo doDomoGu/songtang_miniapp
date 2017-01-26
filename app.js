6aa2ce3a2b18885f056324f04cd73201518cd0ed
doDomoGu <kaka_71936410@qq.com>
2017-01-26 21:33:35 +0800

    用户授权 调试

--- app.js
+++ app.js
@@ -41,24 +41,24 @@ App({
 
   },
 
-  /*getUserInfo222:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
-    }
-  },*/
+  // getUserInfo222:function(cb){
+  //   var that = this
+  //   if(this.globalData.userInfo){
+  //     typeof cb == "function" && cb(this.globalData.userInfo)
+  //   }else{
+  //     //调用登录接口
+  //     wx.login({
+  //       success: function () {
+  //         wx.getUserInfo({
+  //           success: function (res) {
+  //             that.globalData.userInfo = res.userInfo
+  //             typeof cb == "function" && cb(that.globalData.userInfo)
+  //           }
+  //         })
+  //       }
+  //     })
+  //   }
+  // },
   globalData:{
     userInfo:null,
     ucenterUserInfo:null,
--- pages/index/index.js
+++ pages/index/index.js
@@ -14,20 +14,27 @@ Page({
   },
   onLoad: function () {
       wx.login({
-          success: function(res) {
-              if (res.code) {
-                  console.log(res);
+          success: function() {
+              wx.getUserInfo({
+                success: function (res) {
+                    console.log(res);
 
-                  //发起网络请求
-                /*wx.request({
-                 url: 'https://test.com/onLogin',
-                 data: {
-                 code: res.code
-                 }
-                 })*/
-              } else {
-                  console.log('获取用户登录态失败！' + res.errMsg)
-              }
+                    // if (res.code) {
+                    //     console.log(res);
+                    //
+                    //     //发起网络请求
+                    //   /*wx.request({
+                    //    url: 'https://test.com/onLogin',
+                    //    data: {
+                    //    code: res.code
+                    //    }
+                    //    })*/
+                    // } else {
+                    //     console.log('获取用户登录态失败！' + res.errMsg)
+                    // }
+                }
+              })
+
           }
       });
 
@@ -35,7 +42,7 @@ Page({
     //console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    // app.getUserInfo(function(userInfo){
+    // app.getUserInfo222(function(userInfo){
     //   //更新数据
     //   that.setData({
     //     userInfo:userInfo
